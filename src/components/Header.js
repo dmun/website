88e2574db36c0ee0a57388e94fe184511d708f40
dmun <davidru01@live.nl>
2021-12-30 16:25:57 +0100

    Change Header to absolute, hover opacity nav items

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { ReactComponent as ReactLogo } from '../logo.svg';
 
 function Header() {
   return (
-    <nav className='absolute w-full text-white p-5 flex flex-row place-content-between'>
+    <nav className='opacity-50 hover:opacity-100 transition-opacity absolute top-0 z-10 w-full text-white p-5 flex flex-row place-content-between'>
       <a
         className='font-medium text-xl flex flex-row'
         href='/'
@@ -11,10 +11,10 @@ function Header() {
         <ReactLogo className='h-6 sm:h-4 my-auto' />
         <a className='invisible absolute sm:visible sm:relative pt-1'>David van Munster</a>
       </a>
-      <ul className='text-slate-400 flex flex-row my-auto'>
-        <li className='px-2 hover:text-white transition-colors'>About</li>
-        <li className='px-2 hover:text-white transition-colors'>Work</li>
-        <li className='px-2 hover:text-white transition-colors'>Contact</li>
+      <ul className='flex flex-row my-auto'>
+        <li className='px-2 opacity-50 hover:opacity-100 transition-opacity'>About</li>
+        <li className='px-2 opacity-50 hover:opacity-100 transition-opacity'>Work</li>
+        <li className='px-2 opacity-50 hover:opacity-100 transition-opacity'>Contact</li>
       </ul>
     </nav>
   );
